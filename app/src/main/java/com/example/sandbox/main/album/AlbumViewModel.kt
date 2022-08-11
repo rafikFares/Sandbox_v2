43417ec36bc92d2a3e2aac2b051b82e3530537b5
Rafik <rafik.fares@shopmium.com>
2022-08-11 20:39:11 +0200

    init album view model

---
+++ app/src/main/java/com/example/sandbox/main/album/AlbumViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.sandbox.main.album
+
+import android.util.Log
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.sandbox.BuildConfig
+import com.example.sandbox.core.pagging.ImageItemPagingSource
+import com.example.sandbox.core.pagging.ParentPagingSource
+import com.example.sandbox.core.repository.data.ImageItem
+import com.example.sandbox.core.repository.local.LocalRepository
+import com.example.sandbox.main.platform.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel(binds = [AlbumViewModel::class])
+class AlbumViewModel(private val localRepository: LocalRepository) : BaseViewModel() {
+
+    override val uiState: StateFlow<BaseUiState?>
+        get() = TODO("Not yet implemented")
+
+    private val _albumItemsPagingSource = MutableLiveData<ImageItemPagingSource>()
+
+    private val pagingSourceFactory = {
+        val imageItemPagingSource = ImageItemPagingSource(localRepository)
+        _albumItemsPagingSource.value = imageItemPagingSource
+        imageItemPagingSource
+    }
+
+    val albumPager: Flow<PagingData<ImageItem>> = Pager(
+        config = PagingConfig(pageSize = ParentPagingSource.ITEMS_PER_PAGE, enablePlaceholders = false),
+        pagingSourceFactory = pagingSourceFactory
+    ).flow.cachedIn(viewModelScope)
+
+    override fun onCreate(owner: LifecycleOwner) {
+        viewModelScope.launch(Dispatchers.Default) {
+            localRepository.observeDataState().collect {
+                _albumItemsPagingSource.value?.invalidate()
+            }
+        }
+    }
+
+    override fun log(message: String, exception: Exception?) {
+        if (BuildConfig.DEBUG) {
+            Log.d("AlbumViewModel", message, exception)
+        }
+    }
+}
