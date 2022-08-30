41631c48d405e1b0455fb9a64d1af8738f5abcfa
Rafik <rafik.fares@shopmium.com>
2022-08-30 16:04:45 +0200

    improve test Performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,10 @@ tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
+tasks.withType<Test>().configureEach {
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+}
+
 // tasks.withType<Test>().configureEach {
 //     if (name == "connectedDebugAndroidTest") {
 //         ignoreFailures = true
